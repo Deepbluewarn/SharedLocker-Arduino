715debf5167986861b4dc906e303ca6ce3ae0740
Jaehyeon Joo <n5lp97@gmail.com>
2024-05-19 12:41:31 +0900

    Create sharedLocker.ino

---
+++ sharedLocker/sharedLocker.ino
@@ -0,0 +1,152 @@
+#if defined(ARDUINO_PORTENTA_C33)
+#include <WiFiC3.h>
+#elif defined(ARDUINO_UNOWIFIR4)
+#include <WiFiS3.h>
+#endif
+
+#include <PubSubClient.h>
+#include "WiFiSSLClient.h"
+
+// WiFi credentials
+const char *ssid = "SSID";
+const char *pass = "PASS";
+
+// MQTT Broker settings
+const int mqtt_port = 8883;  // MQTT port (TLS)
+const char *mqtt_broker = "r14333b1.ala.eu-central-1.emqxsl.com";  // EMQX broker endpoint
+const char *mqtt_topic_req = "sharedLocker-request";     // MQTT topic
+const char *mqtt_topic_res = "sharedLocker-response";
+const char *mqtt_username = "capstone";  // MQTT username for authentication
+const char *mqtt_password = "1234";  // MQTT password for authentication
+
+// WiFi and MQTT client initialization
+WiFiSSLClient client;
+PubSubClient mqtt_client(client);
+
+int wifiStatus = WL_IDLE_STATUS;
+
+static const char ca_cert[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh
+MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
+d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD
+QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT
+MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j
+b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG
+9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB
+CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97
+nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt
+43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P
+T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4
+gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO
+BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR
+TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw
+DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr
+hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg
+06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF
+PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls
+YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk
+CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=
+-----END CERTIFICATE-----
+)EOF";
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void connectToWiFi(){
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (wifiStatus != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    wifiStatus = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  Serial.println("Connected to WiFi");
+  printWifiStatus();
+}
+
+void connectToMQTT() {
+    client.setCACert(ca_cert);
+    while (!mqtt_client.connected()) {
+        String client_id = "esp8266-client-" + random(300);
+        Serial.println("Connecting to MQTT Broker.....");
+        if (mqtt_client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+            Serial.println("Connected to MQTT broker");
+        } else {
+            Serial.print("Failed to connect to MQTT broker, rc=");
+            Serial.println(mqtt_client.state());
+            delay(5000);
+        }
+    }
+}
+void mqttCallback(char *mqtt_topic, byte *payload, unsigned int length) {
+    Serial.print("Message received on mqtt_topic: ");
+    Serial.println(mqtt_topic);
+    Serial.print("Message: ");
+    for (unsigned int i = 0; i < length; i++) {
+        Serial.print((char) payload[i]);
+    }
+    Serial.println("\n-----------------------");
+}
+
+void setup(){
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  while (!Serial);
+
+  connectToWiFi();
+
+  mqtt_client.setServer(mqtt_broker, mqtt_port);
+  mqtt_client.setCallback(mqttCallback);
+  connectToMQTT();
+  
+}
+
+void loop(){
+  // MQTT Broker 연결
+  if (!mqtt_client.connected()) {
+      connectToMQTT();
+  }
+  mqtt_client.loop();
+
+  while (Serial1.available() > 0) {
+    char buffer[100] = {0};
+
+    String barcode_data = Serial1.readStringUntil('\n');
+    barcode_data.toCharArray(buffer, barcode_data.length());
+
+    Serial.println(barcode_data);
+    
+    mqtt_client.publish(mqtt_topic_req, buffer);
+  }
+}
